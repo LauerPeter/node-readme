66ef3d88a174ff04250f35c4f84891d6d670845a
Peter Lauer <lauerpeter11@gmail.com>
2023-10-04 22:28:14 -0500

    update prompt to follow order and added the correct license image, may come back and add more features

--- index.js
+++ index.js
@@ -1,93 +1,100 @@
-//fs var
+
+
 const fs = require("fs");
-
-//inquirer 
 const inquirer = require("inquirer");
-
-//grabs the readme template
-
 const generateReadme = require("./readmeTemplate");
-
-//prompt questions
 
 inquirer
   .prompt([
     {
-      ///PROJECTNAME
       type: 'input',
       message: 'What will be the name/title of your project?',
       name: 'projectName',
     },
   
     {
-      ///DESCRIPTION
       type: 'input',
       message: 'Please provide a description of your project.',
       name: 'description',
     },
+    
     {
-      ///USAGE
       type: 'input',
-      message:'What is the usage of this application?',
+      message: 'What are the key features of your project?',
+      name: 'features',
+    },
+    
+    {
+      type: 'input',
+      message: 'How will your project be used?',
       name: 'usage',
     },
+    
     {
-      ///CONTRIBUTION
       type: 'input',
-      message: 'What are the contribution guidlines for this application?',
-      name: 'contribution',
+      message: 'What technologies are used in your project?',
+      name: 'technologies',
     },
+    
     {
-      ///INSTALLATION
       type: 'input',
-      message:'Are there any installation instructions for this application?',
-      name: 'userInstructions'
+      message: 'Are there any installation instructions for this application?',
+      name: 'installation',
     },
+    
     {
-      ///TEST INSCRUCTIONS
+      type: 'list',
+      message: 'Please select a license for your project',
+      name: 'license',
+      choices: [
+        'Apache License 2.0',
+        'GNU General Public License v3.0',
+        'MIT License',
+        'BSD 2-Clause "Simplified License"',
+        'BSD 3-Clause "New" or "Revised" License',
+        'Boost Software License 1.0',
+        'Creative Commons Zero v1.0 Universal',
+        'Eclipse Public License 2.0',
+        'GNU Affero General Public License v3.0',
+        'GNU Lesser General Public License v2.1',
+        'Mozilla Public License 2.0',
+        'The Unlicense',
+      ],
+    },
+    
+    {
       type: 'input',
-      message: 'Are there any test instructions for this application?',
-      name: 'testInstruction',
+      message: 'Provide links or resources related to your project (e.g., website, documentation)',
+      name: 'links',
     },
+
     {
-      ///GITHUB USERNAME
+      type: 'input',
+      message: 'Provide a link to a screenshot or demo (If you do not have a link you can manually add it to the generated file)',
+      name: 'screenshot',
+    },
+
+    {
+      type: 'input',
+      message: 'How can others contact you for questions or contributions?',
+      name: 'contact',
+    },
+
+    {
       type: 'input',
       message: 'Please enter your GitHub username',
       name: 'github',
     },
+    
     {
-      ///EMAIL
       type: 'input',
       message: 'Please enter your email linked to your GitHub repo',
       name: 'email',
     },
-    {
-      ///LICENSES - WILL TRY AND IMPLEMENT THE ICON, NOT SURE HOW TO DO THAT
-      type: 'list',
-      message: 'Please select a license for your project',
-      name: 'license',
-      //list of licenses I coppied from Github
-      choices: 
-      ['Apache License 2.0', 
-      'GNU General Public License v3.0', 
-      'MIT License', 
-      'BSD 2-Clause "Simplified License"', 
-      'BSD 3-Clause "New" or "Revised" License', 
-      'Boost SoftWare License 1.0', 
-      'Creative Commons Zero v1.0 Universal', 
-      'Eclipse Public License 2.0', 
-      'GNU Affero General Public License v3.0', 
-      'GNU Lesser General Public License v2.1', 
-      'Mozzila Public License 2.0', 
-      'The Unlicense'],
-    },
   ])
-
   .then((userResponses) => {
-    
     const readmeTemplate = generateReadme(userResponses);
-    ///creates a reamme file from the template on the other file
-    fs.writeFile("READMETest.md", readmeTemplate, (err) => {
+    fs.writeFile("README.md", readmeTemplate, (err) => {
       if (err) {
         console.error("Error writing README file:", err);
       } else {
--- readmeTemplate.js
+++ readmeTemplate.js
@@ -12,8 +12,7 @@ const generateReadme = (userResponses) => {
     github,
     email,
     links,
-    screenshot,
-    contact,
+    screenshot
   } = userResponses;
 
   // Create GitHub and Email links
@@ -21,9 +20,48 @@ const generateReadme = (userResponses) => {
   const emailLink = `[Email](mailto:${email})`;
 
   // Generate license badge based on the selected license
-  const licenseBadge = `![License](https://img.shields.io/badge/License-${encodeURI(
-    license
-  )}-blue.svg)`;
+  let licenseBadge = '';
+
+  switch (license) {
+    case 'Apache License 2.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+      break;
+    case 'GNU General Public License v3.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+      break;
+    case 'MIT License':
+      licenseBadge = '![License](https://img.shields.io/badge/License-MIT-yellow.svg)';
+      break;
+    case 'BSD 2-Clause "Simplified License"':
+      licenseBadge = '![License](https://img.shields.io/badge/License-BSD%202--Clause-blue.svg)';
+      break;
+    case 'BSD 3-Clause "New" or "Revised" License':
+      licenseBadge = '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+      break;
+    case 'Boost Software License 1.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)';
+      break;
+    case 'Creative Commons Zero v1.0 Universal':
+      licenseBadge = '![License](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)';
+      break;
+    case 'Eclipse Public License 2.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-EPL%202.0-red.svg)';
+      break;
+    case 'GNU Affero General Public License v3.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)';
+      break;
+    case 'GNU Lesser General Public License v2.1':
+      licenseBadge = '![License](https://img.shields.io/badge/License-LGPL%20v2.1-blue.svg)';
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseBadge = '![License](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)';
+      break;
+    case 'The Unlicense':
+      licenseBadge = '![License](https://img.shields.io/badge/license-Unlicense-blue.svg)';
+      break;
+    default:
+      licenseBadge = '';
+  }
 
   return `
   # ${projectName}
@@ -50,9 +88,7 @@ const generateReadme = (userResponses) => {
 
   ## License
 
-  ${licenseBadge}
-
-  This project is licensed under the [${license}](LICENSE.txt) license.
+  This project is licensed under the ${licenseBadge} license.
 
   ## Links
 
@@ -60,7 +96,7 @@ const generateReadme = (userResponses) => {
 
   ## Screenshot
 
-  ![Screenshot](screenshot.png)
+  ${screenshot}
 
   ## Contact
 
